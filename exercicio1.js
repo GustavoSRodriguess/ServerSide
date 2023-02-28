ab3c7f7a6d3a0b58f3bb0511c26ab3b32e6df86f
Gustavo <119255970+GustavoSRodriguess@users.noreply.github.com>
2023-02-27 21:46:29 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,6 @@
+let nome = "gustavo";
+let sala = "B";
+let cpf = 47812356915;
+let presente = true;
+
+console.log("bom vindo " + nome + " sua sala e " + sala + " seu cpf e " + cpf + " e vc eta presente? " + presente);
